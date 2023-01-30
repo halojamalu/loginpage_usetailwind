3b37ec7910c976f975a20545001533ee1a8546aa
halojamalu <digital.jamalu@gmail.com>
2023-01-31 06:35:51 +0700

    edit color bg

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -7,8 +7,8 @@ const Register = () => {
   return (
     <div className="antialiased flex w-full min-h-screen justify-center items-center bg-gray-300">
       <div className=" relative flex flex-col md:flex-row  md:space-y-0 bg-blue-50 w-full max-w-3xl p-5 md:p-8 rounded-xl shadow-lg text-slate-50 overflow-hidden ">
-        <div className="absolute w-72 h-72 bg-blue-500 rounded-full -bottom-20 -left-20 blur-3xl"></div>
-        <div className="absolute w-80 h-80 bg-yellow-500 rounded-full -right-20 -top-20 blur-3xl"></div>
+        <div className="absolute w-96 h-96 bg-blue-500 rounded-full -bottom-32 -left-32 blur-3xl"></div>
+        <div className="absolute w-[28rem] h-[28rem] bg-yellow-500 rounded-full -right-20 -top-20 blur-3xl"></div>
         <div className="flex flex-col space-y-8 justify-between z-10 ">
           <div>
             <h1 className=" bg-blue-900 py-2 px-4 rounded-md w-fit font-bold">
